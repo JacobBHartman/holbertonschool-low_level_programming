53d644d48d9ca4c82cb38e90fa7aee9eda5df918
Jacob Hartman <209@holbertonschool.com>
2017-09-13 23:34:57 +0000

    had to split a line that exceed 80 colums

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,8 @@
  */
 int main(void)
 {
-	char buf[61] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
+	char buf[61] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\
+\n";
 	write(2, buf, strlen(buf));
 
 	return (1);
