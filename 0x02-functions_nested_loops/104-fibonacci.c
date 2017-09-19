c56d3fd1a8d7639a420f9e7d8d7482f2bdc6ea1e
JacobBHartman <209@holbertonschool.com>
2017-09-19 18:03:07 +0000

    made 104-fibonacci.c betty-compliant

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,50 +9,39 @@
  */
 int main(void)
 {
-	int n;
-	unsigned long first, second, third, firstA, firstB, secA, secB;
+	unsigned long n, first, second, third, firstA, firstB, secA, secB;
 	unsigned long thirdA, thirdB, carry;
 
-	n = 0;
-	first = 1;
-	second = 2;
+	n = 0; first = 1; second = 2;
 	printf("%lu, %lu, ", first, second);
 	for (n = 0; n < 90; n++)
 	{
 		third = first + second;
 		printf("%lu, ", third);
-		first = second;
-		second = third;
+		first = second; second = third;
 	}
-	firstA = first / 1000000000000;
-	firstB = first % 1000000000000;
-	secA = second / 1000000000000;
-	secB = second % 1000000000000;
+	firstA = first / 1000000000000; firstB = first % 1000000000000;
+	secA = second / 1000000000000; secB = second % 1000000000000;
 	for (n = 0; n < 5; n++)
 	{
 		thirdB = firstB + secB;
 		if (thirdB > 999999999999)
 		{
 			carry = thirdB / 1000000000000;
-			thirdB %= 1000000000000;
-			thirdA = firstA + secA + carry;
+			thirdB %= 1000000000000; thirdA = firstA + secA + carry;
 		}
 		else
 		{
 			thirdA = firstA + secA;
 		}
 		printf("%lu%lu, ", thirdA, thirdB);
-		firstA = secA;
-		firstB = secB;
-		secA = thirdA;
-		secB = thirdB;
+		firstA = secA; firstB = secB; secA = thirdA; secB = thirdB;
 	}
 	thirdB = firstB + secB;
 	if (thirdB > 999999999999)
 	{
 		carry = thirdB / 1000000000000;
-		thirdB %= 1000000000000;
-		thirdA = firstA + secA + carry;
+		thirdB %= 1000000000000; thirdA = firstA + secA + carry;
 	}
 	else
 	{
