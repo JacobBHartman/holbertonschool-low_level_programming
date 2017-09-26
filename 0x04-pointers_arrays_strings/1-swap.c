e5fd10f18feb7c7e36de18c6c2005ec9b9ea50ac
JacobBHartman <209@holbertonschool.com>
2017-09-26 14:32:40 +0000

    attempted task 1

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swap the value of the two integers
+ * @a: first integer to be swapped
+ * @b: secnd integer to be swapped
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *b;
+	*b = *a;
+	*a = temp;
+}
--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -28,5 +28,6 @@ void print_triangle(int size);
 void print_number(int n);
 /* 0x04-pointers_arrays_strings */
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif /* _HOLBERTON_H_ */
