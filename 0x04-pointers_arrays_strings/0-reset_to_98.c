6a7d24e88c10cd363e030dd291f89b5dd5d327f8
JacobBHartman <209@holbertonschool.com>
2017-09-26 14:11:04 +0000

    first attempt at task 0

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -2,4 +2,11 @@
 
 /**
  * reset_to_98 - takes a pointer and updates the value it points to
- * @*n: pointer whose value is the address 
+ * @*n: pointer whose value is the address
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
