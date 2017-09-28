28ecbc3a430ae79b496483686e695163dbf6e8fd
JacobBHartman <209@holbertonschool.com>
2017-09-28 19:01:50 +0000

    initial commit

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be capitalized
+ *
+ * Return: a pointer to a character in an array
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int inWord;
+
+	inWord = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z' && inWord == 0)
+		{
+			inWord = 1;
+			s[i] -= 32;
+		}
+		if (s[i] >= 'A' && s[i] <= 'Z' && inWord == 0)
+			inWord = 1;
+		if (s[i] >= '0' && s[i] <= '9' && inWord == 0)
+			inWord = 1;
+		if (s[i] == ' ' || s[i] == '\n' || s[i] == '-' || s[i] == '.' ||\
+		    s[i] == '\t')
+			inWord = 0;
+	}
+	return (s);
+}
---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * leet_tr - translates into leet
+ * @s: string to be translated
+ *
+ * Return: a pointer to a translated string
+ */
+char *leet_tr(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\n'; i++)
+	{
+
+	}
+}
