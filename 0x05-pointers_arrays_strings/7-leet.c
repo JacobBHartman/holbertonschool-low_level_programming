5beeeeb158ed2c7c16c2156e585dbc678dec1133
JacobBHartman <209@holbertonschool.com>
2017-09-28 19:53:20 +0000

    corrected lack of blank after declartions

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -10,9 +10,9 @@ char *leet(char *s)
 {
 	int i;
 	int j;
-
 	char t1[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
 	char t2[] = {'4', '3', '0', '7', '1'};
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 5; j++)
