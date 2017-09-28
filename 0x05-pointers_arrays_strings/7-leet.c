d3074c0904041a197ad6f21b91f6ef00e69ab837
JacobBHartman <209@holbertonschool.com>
2017-09-28 19:51:53 +0000

    attempted task 7

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,17 +1,25 @@
 #include "holberton.h"
 
 /**
- * leet_tr - translates into leet
+ * leet - translates into leet
  * @s: string to be translated
  *
  * Return: a pointer to a translated string
  */
-char *leet_tr(char *s)
+char *leet(char *s)
 {
 	int i;
+	int j;
 
-	for (i = 0; s[i] != '\n'; i++)
+	char t1[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	char t2[] = {'4', '3', '0', '7', '1'};
+	for (i = 0; s[i] != '\0'; i++)
 	{
-
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == t1[j] || s[i] == t1[j + 5])
+				s[i] = t2[j];
+		}
 	}
+	return (s);
 }
