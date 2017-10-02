a31781827736d6a97ed0b5b760ea3c901a2c0ff6
JacobBHartman <209@holbertonschool.com>
2017-10-02 19:33:45 +0000

    tried to get checker back to 1 bad check

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -11,17 +11,15 @@ char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i;
 	unsigned int j;
-	char *mark;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		mark = &haystack[i];
 		for (j = 0; needle[j] == haystack[i] && needle[j] != '\0'; j++)
 		{
 			i++;
 		}
 		if (needle[j] == '\0')
-			return (mark);
+			return (&haystack[i]);
 	}
 	return (NULL);
 }
