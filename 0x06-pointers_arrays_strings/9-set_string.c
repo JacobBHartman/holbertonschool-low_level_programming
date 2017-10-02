11988da242c84dbe75d29830fbdd086c6d550fb7
JacobBHartman <209@holbertonschool.com>
2017-10-02 16:58:20 +0000

    final task attempted, waiting for checker

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of **s to *to
+ * @s: pointer
+ * @to: char
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
