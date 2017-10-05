912c443d95f8d66a57d0c0fec024b86608f7cf20
JacobBHartman <209@holbertonschool.com>
2017-10-05 16:45:08 +0000

    clean up, attempted 100, but did not finish

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings to see if theyre identical incl. wildcards
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 if identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+	int lens2;
+	unsigned int matching;
+
+	matching = 1;
+	// strlen s1len
+	// strlen s2len
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		
+	}
+}
