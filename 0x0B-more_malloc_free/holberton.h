69e9cfd2885c8c14e9ba93cb60716baa23794733
JacobBHartman <209@holbertonschool.com>
2017-10-12 22:21:45 +0000

    forgot to add header file

---
+++ 0x0B-more_malloc_free/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/* useful functions */
+int _putchar(char c);
+int _strlen(char *s);
+/* Project 0x0B */
+void *malloc_checked(unsigned int b);
+
+#endif /* _HOLBERTON_H_ */
