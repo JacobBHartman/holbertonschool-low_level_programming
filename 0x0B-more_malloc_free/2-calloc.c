76bd2963febf4ec7c6c33a5161c13fc1c79a681b
JacobBHartman <209@holbertonschool.com>
2017-10-12 23:34:36 +0000

    first attempt at checker for task

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: size of elements
+ *
+ * Return: a pointer to an array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		*(ptr + i) = 0;
+	return ((void *)ptr);
+}
