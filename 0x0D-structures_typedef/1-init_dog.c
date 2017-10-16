6d4bf60bc8f8c3416d43a98f1411115e4a9d0afa
JacobBHartman <209@holbertonschool.com>
2017-10-16 20:35:39 +0000

    didnt know I had to add function prototype to dog.h

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
  * init_dog - initializes a variable of type struct dog
@@ -12,8 +11,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+
+	}
 }
