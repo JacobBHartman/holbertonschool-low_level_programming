5acbf6fd9e95f368be9c23f32452bea54639e648
JacobBHartman <209@holbertonschool.com>
2017-10-16 18:48:32 +0000

    initial commit, awaiting checker

---
+++ 0x0C-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef _preprocessor_h_
+#define _preprocessor_h_
+
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _function_like_macro_h_
+#define _function_like_macro_h_
+
+#define ABS(X) (((X) >= 0) ? (X) : -(X))
+
+#endif
