911e71a53152f336c450f9af7c3e303866c8c2e9
JacobBHartman <209@holbertonschool.com>
2017-10-16 20:08:21 +0000

    protected from double inclusion and added struct description

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - characteristics of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: dogs have a name, an age, and an owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
