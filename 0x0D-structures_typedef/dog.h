d966dfd9843ad748e5117f86a795fa129d404a5d
JacobBHartman <209@holbertonschool.com>
2017-10-16 20:50:29 +0000

    now i know that comment style for betty is reserved

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -16,9 +16,7 @@ struct dog
 	char *owner;
 };
 
-/**
- * function prototypes
- */
+/* function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
