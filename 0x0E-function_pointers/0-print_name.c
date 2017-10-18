e0673fc509bb07e5570dd875954c95e726bfc780
JacobBHartman <209@holbertonschool.com>
2017-10-18 20:07:19 +0000

    ya dont need a return in your documentation if its void

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -4,11 +4,10 @@
  * print_name - prints a name
  * @name: the name
  * @f: pointer to a function (that takes string and returns void)
- *
- * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
