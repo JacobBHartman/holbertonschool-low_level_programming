dee9540fe56a68ba1bb58e9d178125a3ae8555c7
JacobBHartman <209@holbertonschool.com>
2017-10-18 20:46:23 +0000

    now accoutning for empty array

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -11,7 +11,8 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-	if (action == NULL)
+
+	if (action == NULL || array == NULL)
 		return;
 	for (i = 0; i < size; i++)
 		action(*(array + i));
