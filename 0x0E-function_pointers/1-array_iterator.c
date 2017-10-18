fd22b9c2a203c6fc046eb8314c3c6db9391e1117
JacobBHartman <209@holbertonschool.com>
2017-10-18 20:38:44 +0000

    accounting for empty array element or empty function pointer

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -13,5 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 	for (i = 0; i < size; i++)
+		if(*(array + i) == NULL || action == NULL)
+			return;
 		action(*(array + i));
 }
