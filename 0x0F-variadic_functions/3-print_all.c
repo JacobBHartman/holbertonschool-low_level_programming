ef10a4183548c0866fb66b4ec1010d30f825b38e
JacobBHartman <209@holbertonschool.com>
2017-10-21 21:51:51 +0000

    fixed style checks

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -2,21 +2,18 @@
 
 /**
  * print_all - prints all the arguments no matter the type
- * @format - a list of times of arguments
+ * @format: a list of times of arguments
  *
  * Return: void
  */
 void print_all(const char * const format, ...)
 {
-	va_list vl;         	/* declare variables */
-	unsigned int i;
-	char *storage;
+	va_list vl; /* declare variables */
+	unsigned int i; char *storage;
 	unsigned int ignore;
 
-	va_start(vl, format);	/* initialize the argument list */
-
-	ignore = 0;
-	i = 0;     	/* switch it son */
+	va_start(vl, format); /* initialize the argument list */
+	ignore = 0; i = 0; /* perform primary function */
 	while (*(format + i))
 	{
 		switch (*(format + i))
@@ -43,17 +40,12 @@ void print_all(const char * const format, ...)
 			break;
 		default:
 			ignore = 1;
-			;
 		}
 		if (*(format + 1 + i) != '\0' && ignore == 0)
 			printf(", ");
 		ignore = 0;
 		i++;
 	}
-
-	/* print new line */
-	printf("\n");
-
-	/* clean up */
-	va_end(vl);
+	printf("\n"); /* print newline */
+	va_end(vl); /* cleanup */
 }
