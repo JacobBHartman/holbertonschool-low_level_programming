33f5106d23f05d32ab2195d1b9fb2f4791bdf1bf
JacobBHartman <209@holbertonschool.com>
2017-10-27 00:05:25 +0000

    added condition for NULL

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -11,9 +11,12 @@ size_t print_list(const list_t *h)
 	int num_nod; /* number of nodes */
 
 	num_nod = 0;
-	while (h !=  NULL)
+	while (h != NULL)
 	{
-		printf("[%i] %s\n", h->len, h->str);
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] %s\n", "(nil)");
 		num_nod++;
 		h = h->next;
 	}
