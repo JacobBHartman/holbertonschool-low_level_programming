04802a8eb5d59b0a624699b49824123f1728bf4e
jacobbhartman <209@holbertonschool.com>
2017-10-31 20:19:08 +0000

    account for if head is passed as NULL

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -13,12 +13,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int i;
 
+	i = 0;
 	if (head == NULL)
 		return (NULL);
-	i = 0;
+	temp = head;
 	while (head != NULL && i <= index)
 	{
-		temp = head;
 		head = head->next;
 		i++;
 	}
