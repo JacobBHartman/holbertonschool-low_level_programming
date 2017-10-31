5410a001d2da42e7b244b000904f05db4733e9a8
jacobbhartman <209@holbertonschool.com>
2017-10-31 17:12:34 +0000

    forgot to change function name when copied from previous task

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
+ * listint_len - returns the number of elemtns
  * @h: the head pointer
  *
  * Return: the number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t num_nod; /* number of nodes */
 
