5eee80c971b5d21158a396d4673cd2bd0f3a0cb7
jacobbhartman <209@holbertonschool.com>
2017-10-31 22:44:27 +0000

    accounting if the list isnt big enough

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,8 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = newnode;
 		return (*head);
 	}
-	/* check if node is inserted at index 0 */
-	if (idx == 0)
+	if (idx == 0) /* check if node is inserted at index 0 */
 	{
 		newnode->next = *head;
 		*head = newnode;
@@ -45,6 +44,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp->next = newnode;
 		return (temp->next);
 	}
+	else if (temp->next == NULL && i < idx)
+		return (NULL);
 	/* we reached the right index, now insert node */
 	newnode->next = temp->next;
 	temp->next = newnode;
