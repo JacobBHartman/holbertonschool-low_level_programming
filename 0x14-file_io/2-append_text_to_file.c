8b8c3af7139197a6fadcd8ef69df61e85a5e6a57
JacobBHartman <209@holbertonschool.com>
2017-11-08 02:59:40 +0000

    initial commit for task

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -9,9 +9,17 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
+	size_t length = strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_RDWR | O_APPEND);
+
+	if (write(fd, text_content, length) < 0)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
