97370e2891e061348ae835d00a32cce8c2a4b722
JacobBHartman <209@holbertonschool.com>
2018-01-13 03:27:15 +0000

    hashtable

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -8,23 +8,23 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-        hash_node_t *pail, *temp;
+	hash_node_t *pail, *temp;
 
-        if (ht != NULL && ht->array != NULL)
+	if (ht != NULL && ht->array != NULL)
 	{
 		for (i = 0; i < ht->size; i++)
 		{
 			pail = (ht->array)[i];
 			while (pail != NULL)
 			{
-                                temp = pail->next;
+				temp = pail->next;
 				free(pail->key);
 				free(pail->value);
 				free(pail);
 				pail = temp;
-                        }
-                }
+			}
+		}
 		free(ht->array);
 		free(ht);
-        }
+	}
 }
