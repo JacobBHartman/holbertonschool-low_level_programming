aada0763e6950f113820d5eca94167c498f04428
JacobBHartman <209@holbertonschool.com>
2018-01-13 02:41:31 +0000

    now compiles

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	/* check to see if key already exists */
 	temp = ht->array[i];
-	while (temp != NULL; temp = temp->next)
+	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
