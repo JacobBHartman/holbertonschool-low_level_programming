4ebe5ca13d25dbf1138a839a7eb9a88eaeee3769
JacobBHartman <209@holbertonschool.com>
2018-02-17 01:40:47 +0000

    initial commit for task

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    perimeter = 0
+    height = len(grid)
+    width = len(grid[0])
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] == 1:
+                if x != 0:
+                    perimeter += 1 - grid[y][x-1]
+                if y != 0:
+                    perimeter += 1 - grid[y-1][x]
+                if x != width - 1:
+                    perimeter += 1 - grid[y][x+1]
+                if y != height - 1:
+                    perimeter += 1 - grid[y+1][x]
+    return perimeter
