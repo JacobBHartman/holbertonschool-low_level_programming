4790ca670db27363d93d4ff362cd9056877d1636
JacobBHartman <209@holbertonschool.com>
2018-03-08 00:28:52 +0000

    initial commit for task

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -10,8 +10,14 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left == &node)
+	if (node->parent->left == NULL)
+		return (NULL);
+	else if (node->parent->right == NULL)
+		return (NULL);
+	else if (node->parent->left == node)
 		return (node->parent->right);
-	else
+	else if (node->parent->right == node)
 		return (node->parent->left);
+	else
+		return (NULL);
 }
