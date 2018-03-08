52f93c287f92a71c135e4132f4b7e511db26df8a
JacobBHartman <209@holbertonschool.com>
2018-03-08 00:39:32 +0000

    update header

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -3,18 +3,18 @@
 
 
 
-/*************
- * Libraries *
- *************/
+/***************
+ *  Libraries  *
+ ***************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 
-/**************
- * Structures *
- **************/
+/****************
+ *  Structures  *
+ ****************/
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -35,34 +35,41 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 
-/***********************
- * Function Prototypes *
- ***********************/
+/*************************
+ *  Function Prototypes  *
+ *************************/
 
-void binary_tree_print(const binary_tree_t *);
-size_t binary_tree_height_v2(const binary_tree_t *tree);
-
+/* tasks 0-4 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 
+/* tasks 5-9 */
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 
+/* tasks 10-14 */
 size_t binary_tree_depth(const binary_tree_t *node);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 
+/* tasks 15-18 */
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+/* extra functions */
+void binary_tree_print(const binary_tree_t *);
+size_t binary_tree_height_v2(const binary_tree_t *tree);
+
+
 
 #endif /* _BINARY_TREES_H_ */
