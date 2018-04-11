ee09b2838cda34b577d33bef15c724df041106a1
JacobBHartman <209@holbertonschool.com>
2018-04-11 16:57:44 -0700

    cant use %zu

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -9,14 +9,15 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	int i;
+	int size_int = size;
 
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size_int; i++)
 	{
-		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
