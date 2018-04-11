2083d7562a3413d575b7bc574d58e288a33123ce
JacobBHartman <209@holbertonschool.com>
2018-04-11 16:55:15 -0700

    betty fix

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -1,11 +1,10 @@
 #include "search_algos.h"
 
 /**
- * linear_search - search for a value in an array of integers using
- *                 a linear search algorithm
- * array: a pointer to the first element of the array
- * size: the number of elements in array
- * value: the value to search for
+ * linear_search - search for a value in an array of integers
+ * @array: a pointer to the first element of the array
+ * @size: the number of elements in array
+ * @value: the value to search for
  * Return: the first index where value is located
  */
 int linear_search(int *array, size_t size, int value)
