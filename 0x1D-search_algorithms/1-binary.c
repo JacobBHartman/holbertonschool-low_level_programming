b00ff48c55ff3a3ddeadeeb74bf570da4024124c
JacobBHartman <209@holbertonschool.com>
2018-04-11 17:04:52 -0700

    read the problems wrong

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -3,9 +3,9 @@
 /**
  * binary_search - search for a value in an array of integers using
  *                 a binary search algorithm
- * array: a pointer to the first element of the array
- * size: the number of elements in array
- * value: the value to search for
+ * @array: a pointer to the first element of the array
+ * @size: the number of elements in array
+ * @value: the value to search for
  * Return: the first index where value is located, -1 otherwise
  */
 int binary_search(int *array, size_t size, int value)
